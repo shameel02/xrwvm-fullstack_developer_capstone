359c7391fade97cbe950a2a8845811614bb5add8
shameel02 <sammostafa39@gmail.com>
2024-05-28 15:03:40 -0400

    Adding changes via theia lab environment to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import register_container from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/regist" element={<register_container />} />
     </Routes>
   );
 }
