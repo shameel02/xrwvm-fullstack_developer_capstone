39a244748a3480e887a42414a23e2c49bc67be7a
shameel02 <sammostafa39@gmail.com>
2024-05-28 17:19:15 -0400

    Adding changes via theia lab environment to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,15 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+admin.site.register(CarModel)
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+admin.site.register(CarMake)
